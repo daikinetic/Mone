1b1c84393879083038e11760b832076c86a9b4f3
daiki_takano <dddaiki.geek@gmail.com>
2022-03-18 19:00:26 +0900

    Data Structuring

--- App.jsx
+++ App.jsx
@@ -1,4 +1,19 @@
-
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
 
+import TagMainScreen from "./src/screens/TagMainScreen";
 
+export default function App () {
+    return (
+        <View style={styles.container}>
+            <TagMainScreen/>
+        </View>
+    );  
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#ffffff'
+    }
+  });
